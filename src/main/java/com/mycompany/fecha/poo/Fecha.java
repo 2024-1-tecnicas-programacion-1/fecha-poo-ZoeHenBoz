7c61922803987e08378479b33c1b599395423901
ZoeHenBoz <zoehenboz20@gmail.com>
2024-04-21 19:44:16 -0500

    Taller POO 1 Zoe Henao Boza

--- src/main/java/com/mycompany/fecha/poo/Fecha.java
+++ src/main/java/com/mycompany/fecha/poo/Fecha.java
@@ -1,5 +1,65 @@
 package com.mycompany.fecha.poo;
 
 public class Fecha {
-    // TODO: Aquí va el código de la clase fechas
+    //Atributos de la clase
+    private int dia;
+    private int mes;
+    private int anio;
+       
+    public Fecha (int dia, int mes, int anio){
+        this.dia = dia;
+        this.mes = mes;
+        this.anio = anio;
+    }//cierro la clase fecha
+
+    //complejidad constante
+    public String fechaCorta(){
+       return String.format("%02d/%02d/%04d", dia,mes,anio);
+    }//cierro el metodo fechaCorta
+    
+    //complejidad constante
+    public boolean validarFecha() {
+        if (dia < 1 || dia > 31) {
+            return false;
+        }//cierro if dia
+
+        if (mes < 1 || mes > 12) {
+            return false;
+        }//cierro if mes
+
+        switch (mes) {
+            case 2:
+                if (anio % 4 == 0 && (anio % 100 != 0 || anio % 400 == 0)) {
+                    if (dia > 29) {
+                        return false;
+                    }//cierro if validacion año biciesto
+                    
+                } else if (dia > 28) {
+                    return false;
+                }//cierro if validacion febrero
+      
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                if (dia > 30) {
+                    return false;
+                }
+ 
+        }//cierro switch validacion meses
+        
+        return true;
+    }//cierro el metodo validarFecha
+        
+    
+    //complejidad constante
+    public String mesLetra (){
+        String[] meses= {"enero","febrero","marzo","abril","mayo","junio","julio","agosto","septiembre","octubre","noviembre","diciembre"};
+        return meses[mes-1];
+    }//cierro el metodo mesLetra
+    
+    //complejidad constante
+    public String fechaLarga(){
+        return String.format("%d de %s de %d", dia, mesLetra(), anio);
+    }//cierro el metodo fechaLarga
 }
--- src/main/java/com/mycompany/fecha/poo/Principal.java
+++ src/main/java/com/mycompany/fecha/poo/Principal.java
@@ -1,5 +1,21 @@
 package com.mycompany.fecha.poo;
+import java.util.Scanner;
 
 public class Principal {
-    // TODO: Aquí va el código de la clase Principal
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int dia = sc.nextInt();
+        int mes = sc.nextInt();
+        int anio = sc.nextInt();
+
+        Fecha fecha = new Fecha(dia, mes, anio);
+
+        if (fecha.validarFecha()) {
+     
+            System.out.println("Formato de fecha larga: " + fecha.fechaLarga());
+        } else {
+            System.out.println("La fecha ingresada no es válida.");
+        }
+    }
 }
